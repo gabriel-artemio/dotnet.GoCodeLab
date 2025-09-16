f5fdb79ecb6c1aa1f1131d812ca00d056cf25935
Gabriel Artemio <43358004+gabriel-artemio@users.noreply.github.com>
2025-09-15 23:51:46 -0300

    ajustes

---
+++ Controllers/CadastroController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace gocodelab.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public ActionResult CadastroPaciente()
+        {
+            return View();
+        }
+        public ActionResult CadastroProfissional()
+        {
+            return View();
+        }
+        public ActionResult CadastroCategoria()
+        {
+            return View();
+        }
+        public ActionResult CadastroGrupoAtendimento()
+        {
+            return View();
+        }
+    }
+}
