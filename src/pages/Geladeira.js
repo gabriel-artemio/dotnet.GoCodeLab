6d9324f50339cdd61edbcaf39fd5b8085bafb0fb
Enzo Monteiro <enzovila.monteiro@gmail.com>
2025-11-26 02:35:02 -0300

    adicionando tabela de alertas

--- src/pages/Geladeira.js
+++ src/pages/Geladeira.js
@@ -13,6 +13,28 @@ export default function Geladeira() {
   const [graficoCalor, setGraficoCalor] = useState(null);
   const [graficoOperacoes, setGraficoOperacoes] = useState(null);
 
+  const [alertasRaw, setAlertasRaw] = useState([]);
+  const [alertasFiltrados, setAlertasFiltrados] = useState([]);
+
+  const [pagina, setPagina] = useState(1);
+  const itensPorPagina = 10;
+
+  const [dataInicio, setDataInicio] = useState("");
+  const [dataFim, setDataFim] = useState("");
+
+  const [filtroAnomalia, setFiltroAnomalia] = useState("Todas");
+
+  const listaAnomalias = [
+    "Todas",
+    "Resfriamento lento",
+    "Porta aberta fora do horário",
+    "Mudança brusca de temperatura",
+    "Porta aberta muito tempo",
+    "Diferença entre sensores",
+    "Temperatura fora da faixa",
+    "Estabilização lenta",
+  ];
+
   const [status, setStatus] = useState("Operando normalmente");
 
   const fetchData = async (endpoint) => {
@@ -21,7 +43,7 @@ export default function Geladeira() {
   };
 
   useEffect(() => {
-    const load = async () => {
+    const loadInicial = async () => {
       const abertura = await fetchData("/grafico/aberturas");
       setGraficoAbertura(abertura);
 
@@ -33,28 +55,35 @@ export default function Geladeira() {
 
       const alertas = await fetchData("/alertas");
 
+      const ordenados = [...alertas].sort(
+        (a, b) =>
+          new Date(b.timestamp.replace(" ", "T")) -
+          new Date(a.timestamp.replace(" ", "T"))
+      );
+
+      setAlertasRaw(ordenados);
+      aplicarFiltros(ordenados);
+
       const tempMax = Math.max(
         ...alertas.map((item) => Math.max(item.temp1, item.temp2))
       );
       const tempMin = Math.min(
         ...alertas.map((item) => Math.min(item.temp1, item.temp2))
       );
-
       const anomalias = alertas.reduce(
         (acc, atual) => acc + atual.anomalias.length,
         0
       );
 
-      // 👉 carrega o gráfico de operações somente uma vez
-      const operacoes = await fetchData("/grafico/operacoes");
-      setGraficoOperacoes(operacoes.data);
-
       setTemperaturaMax(tempMax);
       setTemperaturaMin(tempMin);
       setTotalAnomalia(anomalias);
+
+      const operacoes = await fetchData("/grafico/operacoes");
+      setGraficoOperacoes(operacoes.data);
     };
 
-    load();
+    loadInicial();
 
     const interval = setInterval(async () => {
       const abertura = await fetchData("/grafico/aberturas");
@@ -65,28 +94,49 @@ export default function Geladeira() {
 
       const calor = await fetchData("/grafico/calor");
       setGraficoCalor(calor);
-
-      const alertas = await fetchData("/alertas");
-
-      const tempMax = Math.max(
-        ...alertas.map((item) => Math.max(item.temp1, item.temp2))
-      );
-      const tempMin = Math.min(
-        ...alertas.map((item) => Math.min(item.temp1, item.temp2))
-      );
-
-      const anomalias = alertas.reduce(
-        (acc, atual) => acc + atual.anomalias.length,
-        0
-      );
-
-      setTemperaturaMax(tempMax);
-      setTemperaturaMin(tempMin);
-      setTotalAnomalia(anomalias);
     }, 5000);
 
     return () => clearInterval(interval);
   }, []);
+
+  useEffect(() => {
+    aplicarFiltros(alertasRaw);
+  }, [dataInicio, dataFim, filtroAnomalia]);
+
+  const aplicarFiltros = (lista) => {
+    let filtrado = [...lista];
+
+    if (filtroAnomalia !== "Todas") {
+      filtrado = filtrado.filter((item) =>
+        item.anomalias.includes(filtroAnomalia)
+      );
+    }
+
+    if (dataInicio) {
+      filtrado = filtrado.filter(
+        (item) =>
+          new Date(item.timestamp.replace(" ", "T")) >= new Date(dataInicio)
+      );
+    }
+
+    if (dataFim) {
+      filtrado = filtrado.filter(
+        (item) =>
+          new Date(item.timestamp.replace(" ", "T")) <=
+          new Date(`${dataFim}T23:59:59`)
+      );
+    }
+
+    setAlertasFiltrados(filtrado);
+    setPagina(1);
+  };
+
+  const inicio = (pagina - 1) * itensPorPagina;
+  const fim = inicio + itensPorPagina;
+
+  const alertasPaginados = alertasFiltrados.slice(inicio, fim);
+
+  const totalPaginas = Math.ceil(alertasFiltrados.length / itensPorPagina);
 
   return (
     <>
@@ -96,6 +146,7 @@ export default function Geladeira() {
         <div className="d-flex justify-content-between align-items-center">
           <h3 className="mb-3">Dashboard - Geladeira</h3>
         </div>
+
         <div className="row">
           <div className="col-md-3 mb-3">
             <div className="card shadow-sm">
@@ -133,6 +184,8 @@ export default function Geladeira() {
             </div>
           </div>
         </div>
+
+        {/* GRAFICOS */}
         <div className="card shadow-sm mt-4">
           <div className="card-body">
             {graficoAbertura && (
@@ -150,6 +203,7 @@ export default function Geladeira() {
             )}
           </div>
         </div>
+
         <div className="card shadow-sm mt-4">
           <div className="card-body">
             {graficoEfeito && (
@@ -166,6 +220,7 @@ export default function Geladeira() {
             )}
           </div>
         </div>
+
         <div className="card shadow-sm mt-4 mb-5">
           <div className="card-body">
             {graficoCalor && (
@@ -182,6 +237,7 @@ export default function Geladeira() {
             )}
           </div>
         </div>
+
         <div className="card shadow-sm mt-4 mb-5">
           <div className="card-body">
             {graficoOperacoes && (
@@ -200,6 +256,112 @@ export default function Geladeira() {
                 </div>
               </>
             )}
+          </div>
+        </div>
+
+        <div className="card shadow-sm mt-5 mb-5">
+          <div className="card-body">
+            <h4 className="mb-3">Alertas</h4>
+
+            <div className="row mb-4">
+              <div className="col-md-3">
+                <label className="form-label">Data inicial</label>
+                <input
+                  type="date"
+                  className="form-control"
+                  value={dataInicio}
+                  onChange={(e) => setDataInicio(e.target.value)}
+                />
+              </div>
+
+              <div className="col-md-3">
+                <label className="form-label">Data final</label>
+                <input
+                  type="date"
+                  className="form-control"
+                  value={dataFim}
+                  onChange={(e) => setDataFim(e.target.value)}
+                />
+              </div>
+
+              <div className="col-md-4">
+                <label className="form-label">Tipo de anomalia</label>
+                <select
+                  className="form-select"
+                  value={filtroAnomalia}
+                  onChange={(e) => setFiltroAnomalia(e.target.value)}
+                >
+                  {listaAnomalias.map((an) => (
+                    <option key={an}>{an}</option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
+            <div className="table-responsive">
+              <table className="table table-striped">
+                <thead>
+                  <tr>
+                    <th>Data/Hora</th>
+                    <th>Temp1</th>
+                    <th>Temp2</th>
+                    <th>Porta</th>
+                    <th>Anomalias</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {alertasPaginados.map((a, idx) => (
+                    <tr key={idx}>
+                      <td>{a.timestamp}</td>
+                      <td>{a.temp1}°C</td>
+                      <td>{a.temp2}°C</td>
+                      <td>{a.porta}</td>
+                      <td>
+                        {a.anomalias.map((x, i) => (
+                          <span key={i} className="badge bg-danger me-1">
+                            {x}
+                          </span>
+                        ))}
+                      </td>
+                    </tr>
+                  ))}
+
+                  {alertasFiltrados.length === 0 && (
+                    <tr>
+                      <td colSpan="5" className="text-center text-muted">
+                        Nenhum alerta encontrado nos filtros selecionados.
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>*/
+
+              <nav className="mt-3">
+                <ul className="pagination justify-content-center">
+                  <li className={`page-item ${pagina === 1 ? "disabled" : ""}`}>
+                    <button
+                      className="page-link"
+                      onClick={() => setPagina(pagina - 1)}
+                    >
+                      Anterior
+                    </button>
+                  </li>
+
+                  <li
+                    className={`page-item ${
+                      pagina === totalPaginas ? "disabled" : ""
+                    }`}
+                  >
+                    <button
+                      className="page-link"
+                      onClick={() => setPagina(pagina + 1)}
+                    >
+                      Próxima
+                    </button>
+                  </li>
+                </ul>
+              </nav>
+            </div>
           </div>
         </div>
       </div>
